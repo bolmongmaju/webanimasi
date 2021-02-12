bd4e200a78b6649d56b610059640b6cedc5d561f
kotamobagudev <blackz.wd@gmail.com>
2021-02-13 03:21:21 +0700

    Clean Code 66

---
+++ js/script.js
@@ -0,0 +1,25 @@
+function animasiInfo(){
+    $('#text span').velocity("transition.slideLeftIn", {stagger: 150, complete: function(){
+        animasiButton();
+    }});
+}
+
+function animasiButton(){
+    $('#start').velocity("transition.bounceUpIn")
+                .mouseenter(function(){
+                    $(this).velocity({width: 100});
+                })
+                .mouseleave(function(){
+                    $(this).velocity({width: 125});
+                });
+}
+
+function animasiIntroOut(){
+    $('#start').velocity("transition.whirlOut", {stagger: 150, complete: function(){
+        $('#text').velocity({"font-size":"20px", "top":"95%"}, {duration: 1000});
+    }});
+}
+
+$(document).ready(function(){
+    animasiInfo();
+});
